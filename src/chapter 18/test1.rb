4ee0938b9865dd88a7d2901e82f0f62880acbfae
Mahedi <mahedi_h@yahoo.com>
2015-12-26 16:37:54 +0000

    finish 18th chapter

---
+++ src/chapter 18/test1.rb
@@ -0,0 +1,24 @@
+require 'test/unit'
+
+class TestClass
+  def initialize(aVal)
+    @val = aVal * 10
+  end
+
+  def getVal
+    return @val
+  end
+end
+
+class MyTest < Test::Unit::TestCase
+  def test1
+    t = TestClass.new(10)
+    assert_equal(100, t.getVal)
+    assert_equal(100, t.getVal)
+    assert(100 != t.getVal)
+  end
+
+  def test2
+    assert_equal(1000, TestCase.new(100).getVal)
+  end
+end
